43ae0e6301189ee9ca777440b6ce6c1cab22b856
Shivaani168 <73694690+Shivaani168@users.noreply.github.com>
2022-06-21 09:50:52 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -2,13 +2,13 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-apiKey: "AIzaSyDlXvfkB_N9AgSj6k20daTbJ-wcLTmnUSk",
-  authDomain: "project-67-e5ef1.firebaseapp.com",
-  databaseURL: "https://project-67-e5ef1-default-rtdb.firebaseio.com",
-  projectId: "project-67-e5ef1",
-  storageBucket: "project-67-e5ef1.appspot.com",
-  messagingSenderId: "772869482645",
-  appId: "1:772869482645:web:f158432b3b7bbbb767939c"
+        apiKey: "AIzaSyDlXvfkB_N9AgSj6k20daTbJ-wcLTmnUSk",
+        authDomain: "project-67-e5ef1.firebaseapp.com",
+        databaseURL: "https://project-67-e5ef1-default-rtdb.firebaseio.com",
+        projectId: "project-67-e5ef1",
+        storageBucket: "project-67-e5ef1.appspot.com",
+        messagingSenderId: "772869482645",
+        appId: "1:772869482645:web:f158432b3b7bbbb767939c"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
